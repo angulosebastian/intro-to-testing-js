5236637adf83232570edf3b8e37e6eb163776d50
Sebastian Angulo <sebastianangulo442@yahoo.com>
2019-08-10 13:10:40 -0500

    add code

--- code.js
+++ code.js
@@ -1,4 +1,13 @@
 // helloWorld function 
-function helloWorld() {
+// function statement syntax functionhelloWorld(){
+//     return "Hello, World!";}
+// function expression syntax (assigning an anonymous function to a variable)
+const helloWorld = function() {
     return "Hello, World!";
 }
+// function sum(a, b){
+//     if (b === "sheep"){
+//         return false;
+//     }
+//     return a + b;
+// }
